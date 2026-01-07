ae187a946c6f4c2c321b053e7d40bb2beb5557ec
Anderson <andersonmoal06@gmail.com>
2026-01-07 09:23:21 -0600

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // Product product = new Product("Laptop", 999.99);
+        Product p2 = new FoodProduct("Leche", 1200, "2024-12-31", true, ProductStatus.DISCONTINUED);
+        ElectronicProduct p3 = new ElectronicProduct("Mouse", 1200, ProductStatus.AVAILABLE);
+
+        System.out.println(p2);
+        System.out.println(p3.getName());
+        System.out.println("p2.isAvailable(): " + p2.isAvailable());
+        System.out.println("p3.isAvailable(): " + p3.isAvailable());
+
+        System.out.println(p3.canBeReturned(10));
+
+        // System.out.println(product);
+        // System.out.println("Product Name: " + product.getName());
+        // System.out.println("Product Price: " + product.getPrice());
+
+        // System.out.println("Tax: " + Product.calculateTax(product.getPrice()));
+
+        System.out.println("Discount: " + p2.calculateDiscount());
+        System.out.println("Discount: " + p3.calculateDiscount());
+
+    }
+
+}
