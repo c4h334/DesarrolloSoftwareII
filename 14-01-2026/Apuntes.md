bd456502973bc0a49a30f6fba5fc953cfe11173e
c4h334 <anderson.monge@ucr.ac.cr>
2026-01-14 13:02:55 -0600

    Delete 14-01-2026 directory

--- 14-01-2026/Apuntes.md
+++
@@ -1,9 +0,0 @@
-# Eliminar elemento de un arreglo
-
-| A | B | C | D | E |
-
-remove(C)
-
-| A | B |   | D | E |
-
-| A | B | D | E |   |
--- 14-01-2026/PracticaGuiada2/src/IOManager.java
+++
@@ -1,7 +0,0 @@
-package PracticaGuiada2.src;
-
-public class IOManager {
-    public static void printMessage(String message) {
-        System.out.println(message);
-    }
-}
