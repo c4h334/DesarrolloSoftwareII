25eea36fd3f7b823f9ab5d85bfa9065ac8e39b57
c4h334 <anderson.monge@ucr.ac.cr>
2026-01-19 08:15:41 -0600

    Práctica arreglos 14-01-2026

---
+++ 14-01-2026/PracticaArreglos/IOManager.java
@@ -0,0 +1,12 @@
+package PracticaArreglos;
+
+public class IOManager {
+
+    static Object read(Object var) {
+        return var;//if (var == String.class){}
+    }
+
+    static void write(Object var) {
+        System.out.println(var);
+    }
+}
