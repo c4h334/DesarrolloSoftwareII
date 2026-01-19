802839e0f49adfe0c1315bcce062cbd5383d7056
c4h334 <anderson.monge@ucr.ac.cr>
2026-01-19 15:10:48 -0600

    19-01-2026

---
+++ 19-01-2026/Matrices/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] matriz = new int[4][4];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println(matriz[i][j] + "");
+            }
+            System.out.println();
+        }
+
+        for (int[] fila : matriz) {
+            for (int valor : fila) {
+                System.out.print(valor + " ");
+            }
+            System.out.println();
+        }
+    }
+}
