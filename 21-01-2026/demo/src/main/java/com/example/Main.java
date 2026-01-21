c270c4dc2ebfe2faef0e1dc56d6a5097d54d3f3b
Anderson <andersonmoal06@gmail.com>
2026-01-21 12:19:54 -0600

    Ahora sí, corregido

--- 21-01-2026/demo/src/main/java/com/example/Main.java
+++ 21-01-2026/demo/src/main/java/com/example/Main.java
@@ -1,10 +1,9 @@
-package com.code;
+package com.example;
 
 import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Gson gson = new Gson();
         Person person = new Person("Anderson", 19, true);
         String info = gson.toJson(person);
--- 21-01-2026/demo/src/main/java/com/example/Person.java
+++ 21-01-2026/demo/src/main/java/com/example/Person.java
@@ -1,3 +1,5 @@
+package com.example;
+
 public class Person {
     private String name;
     private int age;
