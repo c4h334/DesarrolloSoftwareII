ed822ca8e6cc207182b6840debbedd0e4b768eb9
Anderson <andersonmoal06@gmail.com>
2026-01-21 11:00:27 -0600

    mañana del 21

---
+++ 21-01-2026/demo/src/main/java/com/code/Main.java
@@ -0,0 +1,12 @@
+package com.code;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Gson gson = new Gson();
+        Person person = new Person("Anderson", 19, true);
+        String info = gson.toJson(person);
+    }
+}
