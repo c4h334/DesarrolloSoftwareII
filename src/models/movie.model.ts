97e2821e3b8aa876070163a538c2c7d32982fc54
c4h334 <anderson.monge@ucr.ac.cr>
2026-02-11 11:27:20 -0600

    Delete src directory

--- src/models/movie.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Movie {
-  id: string;
-  title: string;
-  description: string;
-  posterurl: string;
-  rating: number;
-}
--- src/shared/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="bg-gray-800 text-center py-4 text-gray-400">
-      © 2026 MovieApp - Todos los derechos reservados
-    </footer>
-  );
-}
